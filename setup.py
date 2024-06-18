9b2dff5e69a11fadda27ae45d1705cf5a84459fb
t.uehara <t.uehara@gmail.com>
2024-06-18 22:29:59 +0900

    prepare to release 0.0.7

--- setup.py
+++ setup.py
@@ -1,36 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='elegantt',
-    version='0.0.6',
+    name="elegantt",
+    version="0.0.7",
     packages=find_packages(),
-
-    author='Takayuki Uehara',
-    author_email='t.uehara@gmail.com',
-
-    url='http://github.com/usop4/elegantt',
-
-    description='This is a gantt chart drawing library',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-
-    python_requires='~=3.8',
-
+    author="Takayuki Uehara",
+    author_email="t.uehara@gmail.com",
+    url="http://github.com/usop4/elegantt",
+    description="This is a gantt chart drawing library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires="~=3.8",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Operating System :: OS Independent',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
     ],
-
-    install_requires=[
-        'Pillow>=9.4,<10.4',
-        'Pandas'
-    ],
-
+    install_requires=["Pillow>=9.4,<10.4", "Pandas"],
     entry_points="""
        [console_scripts]
        elegantt = elegantt.command:main
     """,
-
 )
